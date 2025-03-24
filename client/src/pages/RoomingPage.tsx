ce34e4d4e2544fda282247769d810fcd24cb9413
A.V.G.M.Chathuranga <avgmadhushanchathuranga99@gmail.com>
2025-03-24 09:32:50 +0530

    Update RoomingPage.tsx

--- client/src/pages/RoomingPage.tsx
+++ client/src/pages/RoomingPage.tsx
@@ -186,7 +186,10 @@ const RoomingPage = () => {
             </div>
 
             <div
-              onClick={() => setActiveTab(1)}
+              onClick={() => {
+                setActiveTab(1);
+                navigate("/commercial");
+              }}
               className={`w-full sm:w-[176px] h-[80px] sm:h-[64px] bg-gradient-to-l gap-3 group cursor-pointer ${
                 activeTab === 1
                   ? "to-[#FE8930] from-[#FFB05F]"
